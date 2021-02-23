24a962fb2645ad17985cf77377303c643dc20346
taeganwarren <warretaega@gmail.com>
2021-02-22 18:07:17 -0800

    Simplified build process

--- generate.js
+++ generate.js
@@ -1,16 +1,12 @@
-const Handlebars = require('handlebars');
 const fs = require('fs');
+const Handlebars = require('handlebars');
 
-const links = require('./links.json');
+const render = (filename, links) => {
+    const source = fs.readFileSync(filename, 'utf-8').toString();
+    const template = Handlebars.compile(source);
+    const output = template({ links: links});
+    return output;
+}
 
-const link_partial = require('./src/link.handlebars');
-const main_layout = require('./src/main.handlebars');
-const linkPage = require('./src/linkPage.handlebars');
-
-Handlebars.registerPartial('link', link_partial);
-
-fs.writeFileSync('public/index.html', main_layout({
-    body: linkPage({
-        links: links
-    })
-}, 'utf-8'));
+const links = JSON.parse(fs.readFileSync("./links.json", 'utf8'));
+fs.writeFileSync('public/index.html', render('./src/index.handlebars', links), 'utf-8');
