4011b7e9135bfe4650c2d28f7edcf3c7dc4ab26c
taeganwarren <warretaega@gmail.com>
2021-02-22 19:17:58 -0800

    Fixed file paths

--- src/generate.js
+++ src/generate.js
@@ -4,9 +4,9 @@ const Handlebars = require('handlebars');
 const render = (filename, links) => {
     const source = fs.readFileSync(filename, 'utf-8').toString();
     const template = Handlebars.compile(source);
-    const output = template({ links: links});
+    const output = template({ links: links });
     return output;
 }
 
-const links = JSON.parse(fs.readFileSync("./links.json", 'utf8'));
-fs.writeFileSync('public/index.html', render('./src/index.handlebars', links), 'utf-8');
+const links = JSON.parse(fs.readFileSync('src/links.json', 'utf8'));
+fs.writeFileSync('public/index.html', render('src/index.handlebars', links), 'utf-8');
